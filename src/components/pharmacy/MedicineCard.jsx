cac7edd6c911dcb354d3b2e4c954e0616768eef7
Mudit Pandey <163019652+Mudit-2805@users.noreply.github.com>
2025-05-05 16:35:51 +0530

    pharmacy card 1,2 update

--- src/components/pharmacy/MedicineCard.jsx
+++ src/components/pharmacy/MedicineCard.jsx
@@ -16,7 +16,7 @@ const MedicineCard = ({ medicine }) => {
         <p className="medicine-description">{description}</p>
         
         <div className="medicine-price-row">
-          <div className="medicine-price">${price.toFixed(2)}</div>
+          <div className="medicine-price">₹{price.toFixed(2)}</div>
           <button className="add-to-cart-btn">Add to Cart</button>
         </div>
       </div>
